975ca8c8677b3c5500d6d25642e6bab2551d6b72
jhs512 <jangka512@gmail.com>
2025-11-20 10:49:25 +0900

    2 : b

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -1,12 +1,39 @@
 package com.ll.simpleDb;
 
-public class SimpleDb {
-    public SimpleDb(String host, String username, String password, String dbName) {
-    }
+import lombok.Setter;
 
-    public void setDevMode(boolean mode) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SimpleDb {
+    private final String url;
+    private final String username;
+    private final String password;
+
+    @Setter
+    private boolean devMode;
+
+    public SimpleDb(String host, String username, String password, String dbName) {
+        this.url = String.format("jdbc:mysql://%s:3306/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Seoul", host, dbName);
+        this.username = username;
+        this.password = password;
     }
 
     public void run(String sql) {
+        if (devMode) {
+            System.out.println("SQL: " + sql);
+        }
+
+        // try-with-resources를 사용하여 Connection과 Statement를 자동으로 닫습니다.
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
