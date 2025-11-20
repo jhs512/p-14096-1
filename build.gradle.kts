635c29f08fabd486a1e3a7e0271d15c361177c21
jhs512 <jangka512@gmail.com>
2025-11-20 10:22:26 +0900

    work

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,16 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    runtimeOnly("com.mysql:mysql-connector-j:9.5.0")
+
+    testImplementation("org.assertj:assertj-core:3.27.6")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
